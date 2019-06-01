1c1b3cc182d65e994ad05613aa8d72b9eba9853c
Gil4 <Gil44liG@gmail.com>
2019-06-01 17:18:25 +0300

    Landing update

--- assets/scripts/bundle.js
+++ assets/scripts/bundle.js
@@ -1,1 +1,43 @@
-!function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={exports:{},id:o,loaded:!1};return e[o].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var n={};t.m=e,t.c=n,t.p="",t(0)}([function(e,t){!function(){function e(e){e.preventDefault();var t=new XMLHttpRequest,n=document.getElementById("subscribeForm");t.open("POST","send.php",!0),t.onreadystatechange=function(){4===t.readyState&&200===t.status&&console.log("OK!")},t.onerror=function(){alert("Извините, данные не были переданы")},t.send(new FormData(n))}function t(){document.getElementById("btn").addEventListener("click",e)}document.addEventListener("DOMContentLoaded",function(){t()})}()}]);
+! function (e)
+{
+	function t(o)
+	{
+		if (n[o]) return n[o].exports;
+		var r = n[o] = {
+			exports:
+			{},
+			id: o,
+			loaded: !1
+		};
+		return e[o].call(r.exports, r, r.exports, t), r.loaded = !0, r.exports
+	}
+	var n = {};
+	t.m = e, t.c = n, t.p = "", t(0)
+}([function (e, t)
+{
+	! function ()
+	{
+		function e(e)
+		{
+			e.preventDefault();
+			var t = new XMLHttpRequest,
+				n = document.getElementById("subscribeForm");
+			t.open("POST", "send.php", !0), t.onreadystatechange = function ()
+			{
+				4 === t.readyState && 200 === t.status && console.log("OK!")
+			}, t.onerror = function ()
+			{
+				alert("An error has occured during data transfer.")
+			}, t.send(new FormData(n))
+		}
+
+		function t()
+		{	
+			document.getElementById("mc-embedded-subscribe btn").addEventListener("click", e)
+		}
+		document.addEventListener("DOMContentLoaded", function ()
+		{
+			t()
+		})
+	}()
+}]);
